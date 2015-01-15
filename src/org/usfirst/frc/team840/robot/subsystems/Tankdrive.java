1b20f4dd482df553f690c557cbec82c77f18fe05
KenTheWhaleGoddess <kenpreiser@gmail.com>
2015-01-14 17:04:16 -0800

    made pretty

--- src/org/usfirst/frc/team840/robot/subsystems/Tankdrive.java
+++ src/org/usfirst/frc/team840/robot/subsystems/Tankdrive.java
@@ -1,15 +1,10 @@
 package org.usfirst.frc.team840.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-//other import-ant stuff you need
 
-/**
- *
- */
+//to be implemented in the OperatorControl() method.
+
 public class Tankdrive extends Subsystem {
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
 
     public void Tankdrive() {
         lTalon.set(lStick.getY());
