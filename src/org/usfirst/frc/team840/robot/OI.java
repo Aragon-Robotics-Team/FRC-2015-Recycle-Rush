766f43e26c1668cd85e2761e2def3be8f93c3c71
mpolson64 <mpolson64@gmail.com>
2015-01-20 18:12:34 -0800

    Was not working
    Now is working, deleted all class files that were destroying the code
    with a null pointer exception.

--- src/org/usfirst/frc/team840/robot/OI.java
+++ src/org/usfirst/frc/team840/robot/OI.java
@@ -2,8 +2,6 @@ package org.usfirst.frc.team840.robot;
 
 import org.usfirst.frc.team840.robot.subsystems.DrivetrainTank;
 import org.usfirst.frc.team840.robot.commands.TankDrive;
-import org.usfirst.frc.team840.robot.commands.TankDriveCreep;
-import org.usfirst.frc.team840.robot.commands.TankDriveStraight;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
@@ -18,23 +16,19 @@ public class OI {
 	
 	private Joystick joyLeft = new Joystick(0);
 	private Joystick joyRight = new Joystick(1);
-	private JoystickButton triggerLeft = new JoystickButton(joyLeft, 1);
-	private JoystickButton triggerRight = new JoystickButton(joyRight, 1);
-	private JoystickButton left2 = new JoystickButton(joyLeft, 2);
-	private JoystickButton right2 = new JoystickButton(joyRight, 2);
-	
-	public OI() {
-		triggerLeft.whileHeld(new TankDriveCreep(true, RobotMap.CREEP_SPEED));
-		triggerRight.whileHeld(new TankDriveCreep(false, RobotMap.CREEP_SPEED));
-		left2.whileHeld(new TankDriveStraight());
-		right2.whileHeld(new TankDriveStraight());
-		
-	}
 	
 	public Joystick getJoyLeft() {
 		return joyLeft;
 	}
 	public Joystick getJoyRight() {
 		return joyRight;
+	}
+	
+	public OI() {
+		JoystickButton triggerRight = new JoystickButton(joyRight, 1);
+		JoystickButton left2 = new JoystickButton(joyLeft, 2);
+		JoystickButton triggerLeft = new JoystickButton(joyLeft, 1);
+		JoystickButton right2 = new JoystickButton(joyRight, 2);
+		
 	}
 }
