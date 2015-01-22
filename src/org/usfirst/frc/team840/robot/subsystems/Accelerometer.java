11eeb58b2a1e77b5a5961b9a03608b72de46c7ef
Chris Xiong <chrisxiongyang@yahoo.com>
2015-01-22 14:23:24 -0800

    Create Accelerometer.java

---
+++ src/org/usfirst/frc/team840/robot/subsystems/Accelerometer.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team840.robot.subsystems;
+
+import org.usfirst.frc.team840.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *	Drivetrain for the robot. It is unsure at this time what the final drivetrain will be, so we will program one using standard
+ *	two motor tank. This drivtrain uses the TalonSRX motor controllers. At this time, there are no sensors.
+ */
+public class Accelerometer extends Subsystem {
+    
+	private BuiltInAccelerometer acc;
+	
+	public Accelerometer() {
+		super();
+		acc = new BuiltInAccelerometer(RobotMap.accRange);
+	}
+	
+    public void initDefaultCommand() {
+        setDefaultCommand();
+    }
+    
+    public double getXAcc() {
+        return acc.getX();
+    }
+    
+    public double getYAcc() {
+        return acc.getY();
+    }
+    
+    public double getZAcc() {
+        return acc.getZ();
+    }
+}
