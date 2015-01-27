d5267e9fa2c2bc37c4c85ec82c2e3224f61e47ee
Miles P. Olson <mpolson64@gmail.com>
2015-01-26 23:15:16 -0800

    Created classes for Gamepad and GamepadButton
    These two objects are for easier use of the Logitechs. The Gamepad class
    extends Joystick and labels things nicely, while the GamepadButton class
    allows the creation of Buttons for command based programming using the
    gamepad names. So far, only the assignments I know off the top of my
    head have been added.

---
+++ src/edu/art/frc/lib/util/Gamepad.java
@@ -0,0 +1,66 @@
+package edu.art.frc.lib.util;
+
+import edu.wpi.first.wpilibj.Joystick;
+/*
+ * Gamepad class for easier interface with a Logitech gamepad. Names functions in intuitive ways for easy use.
+ */
+
+public class Gamepad extends Joystick {
+
+	public Gamepad(int port) {
+		super(port);
+	}
+
+	
+	//4 axis on the dual thumb-sticks
+	public double getLeftX() {
+		return getRawAxis(1);
+	}
+	
+	public double getLeftY() {
+		return getRawAxis(2);
+	}
+	
+	public double getRightX() {
+		return getRawAxis(4);
+	}
+	
+	public double getRightY() {
+		return getRawAxis(5);
+	}
+	
+	//Face buttons
+	public boolean getButtonA() {
+		return getRawButton(2);
+	}
+	
+	public boolean getButtonB() {
+		return getRawButton(3);
+	}
+
+	public boolean getButtonX() {
+		return getRawButton(1);
+	}
+
+	public boolean getButtonY() {
+		return getRawButton(4);
+	}
+	
+	
+	//Back of controller
+	public boolean getLeftBumper() {
+		return getRawButton(5);
+	}
+	
+	public boolean getLeftTrigger() {
+		return getRawButton(7);
+	}
+	
+	public boolean getRightBumper() {
+		return getRawButton(6);
+	}
+	
+	public boolean getRightTrigger() {
+		return getRawButton(8);
+	}
+}
---
+++ src/edu/art/frc/lib/util/GamepadButton.java
@@ -0,0 +1,40 @@
+package edu.art.frc.lib.util;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+/*
+ * To be used in conjunction with our Gamepad object. This allows for the creation of Buttons, which are used in commmand based programming.
+ */
+
+public class GamepadButton extends JoystickButton {
+
+	public GamepadButton(GenericHID gamepad, String buttonName) {
+		super(gamepad, buttonNumber(buttonName));
+	}
+	
+	private static int buttonNumber(String buttonName) {
+		switch(buttonName) {
+		case "A":
+			return 2;
+		case "B":
+			return 3;
+		case "X":
+			return 1;
+		case "Y":
+			return 4;
+		case "LEFT_BUMPER":
+			return 5;
+		case "LEFT_TRIGGER":
+			return 7;
+		case "RIGHT_BUMPER":
+			return 6;
+		case "RIGHT_TRIGGER":
+			return 8;
+		default:
+			return -1;
+		
+		}
+	}
+
+}
