5109556a5198f2adfc326cf605a9dc2e2145367d
Miles P. Olson <mpolson64@gmail.com>
2015-01-27 16:14:05 -0800

    Added and implemented lerp
    Added an Interpolation abstract class in out library, then used it in
    the arcade drive command.

--- src/edu/art/frc/recycleRush/robot/commands/ArcadeDrive.java
+++ src/edu/art/frc/recycleRush/robot/commands/ArcadeDrive.java
@@ -3,22 +3,38 @@ package edu.art.frc.recycleRush.robot.commands;
 import edu.art.frc.recycleRush.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
+import edu.art.frc.lib.util.Interpolation;
+
 /**
  *
  */
 public class ArcadeDrive extends Command {
 
+	private double currentPower;
+	private double targetPower;
+	private double currentTurn;
+	private double targetTurn;
+	
     public ArcadeDrive() {
         requires(Robot.drivetrain);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	currentPower = 0;
+    	currentTurn = 0;
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.drivetrain.arcadeDrive(Robot.oi.getGamepad().getLeftY() * -1, Robot.oi.getGamepad().getRightX() * -1);	//Inverted for easier driving
+    	targetPower = Robot.oi.getGamepad().getLeftY();
+    	targetTurn = Robot.oi.getGamepad().getRightY();
+    	
+    	//TODO Tune percent
+    	Robot.drivetrain.arcadeDrive(Interpolation.lerp(currentPower, targetPower, .25) * -1, Interpolation.lerp(currentTurn, targetTurn, .25) * -1);	//Inverted for easier driving
+    	
+    	currentPower = Interpolation.lerp(currentPower, targetPower, .25);
+    	currentTurn = Interpolation.lerp(currentTurn, targetTurn, .25);
     }
 
     // Make this return true when this Command no longer needs to run execute()
