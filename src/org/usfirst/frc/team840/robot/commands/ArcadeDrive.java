44cf6e90840d3313d5391a53f52c477245912a87
Chris Xiong <chrisxiongyang@yahoo.com>
2015-02-04 16:05:00 -0800

    Cleaned up ArcadeDrive

--- src/org/usfirst/frc/team840/robot/commands/ArcadeDrive.java
+++ src/org/usfirst/frc/team840/robot/commands/ArcadeDrive.java
@@ -15,33 +15,31 @@ public class ArcadeDrive extends Command {
 	private double lerpedPower;
 	private double lerpedTurn;
 	
-public ArcadeDrive() {
+	public ArcadeDrive() {
         requires(Robot.drivetrain);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	SmartDashboard.putString("Driving mode: ", "Standard");
-    	currentPower = 0;
-    	currentTurn = 0;
+		SmartDashboard.putString("Driving mode: ", "Standard");
+		currentPower = 0;
+		currentTurn = 0;
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	targetPower = Robot.oi.getGamepad().getLeftY();
-    	if (Math.abs(targetPower) < .0625) targetPower = 0;
-    	targetTurn = Robot.oi.getGamepad().getRightX();
-    	if (Math.abs(targetTurn) < .0625) targetTurn = 0;
+		targetPower = Robot.oi.getGamepad().getLeftY();
+		if (Math.abs(targetPower) < .0625) targetPower = 0;
+		targetTurn = Robot.oi.getGamepad().getRightX();
+		if (Math.abs(targetTurn) < .0625) targetTurn = 0;
     	
-    	lerpedPower = Interpolation.lerp(currentPower, targetPower, Robot.lerpFactor);
-    	lerpedTurn = Interpolation.lerp(currentTurn, targetTurn, Robot.lerpFactor);
-    	
-    	//Inverted for easier driving
-    	
-    	Robot.drivetrain.arcadeDrive(lerpedPower * -1, lerpedTurn * -1);
-    	
-    	currentPower = lerpedPower;
-    	currentTurn = lerpedTurn;
+		lerpedPower = Interpolation.lerp(currentPower, targetPower, Robot.lerpFactor);
+		lerpedTurn = Interpolation.lerp(currentTurn, targetTurn, Robot.lerpFactor);
+		
+		Robot.drivetrain.arcadeDrive(lerpedPower * -1, lerpedTurn * -1); //Inverted for easier driving
+		
+		currentPower = lerpedPower;
+		currentTurn = lerpedTurn;
     }
 
     // Make this return true when this Command no longer needs to run execute()
