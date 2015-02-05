3dc94cdb59a8e0ca160f8ef5cb4d066ac4d4de0c
SourMongoose <chrisxiongyang@yahoo.com>
2015-02-05 15:34:52 -0800

    Added SlideIn and SlideOut
    SlideIn and SlideOut are commands that use the DoubleSolenoid to slide
    the tote-picker-upper back and forth.

--- src/org/usfirst/frc/team840/robot/OI.java
+++ src/org/usfirst/frc/team840/robot/OI.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team840.robot;
 
-import org.usfirst.frc.team840.robot.commands.ArcadeDriveCreep;
+//import org.usfirst.frc.team840.robot.commands.ArcadeDriveCreep;
+import org.usfirst.frc.team840.robot.commands.*;
 
 import edu.art.frc.lib.util.Gamepad;
 import edu.art.frc.lib.util.GamepadButton;
@@ -21,7 +22,23 @@ public class OI {
 		GamepadButton leftStick = new GamepadButton(gamepad, "LEFT_JOYSTICK");
 		GamepadButton rightStick = new GamepadButton(gamepad, "RIGHT_JOYSTICK");
 		
+		GamepadButton x = new GamepadButton(gamepad, "X");
+		GamepadButton b = new GamepadButton(gamepad, "B");
+		GamepadButton y = new GamepadButton(gamepad, "Y");
+		GamepadButton a = new GamepadButton(gamepad, "A");
+		
+		GamepadButton leftBumper = new GamepadButton(gamepad, "LEFT_BUMPER");
+		GamepadButton rightBumper = new GamepadButton(gamepad, "RIGHT_BUMPER");
+		
 		leftStick.toggleWhenActive(new ArcadeDriveCreep());
 		rightStick.toggleWhenActive(new ArcadeDriveCreep());
+		
+		x.whenPressed(new SlideIn());
+		b.whenPressed(new SlideOut());
+		y.whenPressed(new SetLift(9001));
+		a.whenPressed(new SetLift(0));
+		
+		leftBumper.whileHeld(new SetLift(Robot.stacker.getPosition() - 1));
+		rightBumper.whileHeld(new SetLift(Robot.stacker.getPosition() + 1));
 	}
 }
