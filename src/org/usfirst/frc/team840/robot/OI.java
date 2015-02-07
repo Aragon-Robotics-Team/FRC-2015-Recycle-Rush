17210369979173a38d96d3db22e41ed6ae2bdd93
mpolson64 <mpolson64@gmail.com>
2015-02-07 09:14:42 -0800

    Removed variable positioning OI functions
    The functions assumed the wrong purpose of the getPosition() method, and
    thus would not have worked. I will re-add something with similar
    functionality once the correct data has been measured.

--- src/org/usfirst/frc/team840/robot/OI.java
+++ src/org/usfirst/frc/team840/robot/OI.java
@@ -26,19 +26,13 @@ public class OI {
 		GamepadButton b = new GamepadButton(gamepad, "B");
 		GamepadButton y = new GamepadButton(gamepad, "Y");
 		GamepadButton a = new GamepadButton(gamepad, "A");
-		
-		GamepadButton leftBumper = new GamepadButton(gamepad, "LEFT_BUMPER");
-		GamepadButton rightBumper = new GamepadButton(gamepad, "RIGHT_BUMPER");
-		
+				
 		leftStick.toggleWhenActive(new ArcadeDriveCreep());
 		rightStick.toggleWhenActive(new ArcadeDriveCreep());
 		
 		x.whenPressed(new SlideIn());
 		b.whenPressed(new SlideOut());
 		y.whenPressed(new SetLiftEncoder(24));
-		a.whenPressed(new SetLiftEncoder(0));
-		
-		leftBumper.whileHeld(new SetLiftEncoder(Robot.stacker.getPosition() - 1));
-		rightBumper.whileHeld(new SetLiftEncoder(Robot.stacker.getPosition() + 1));
+		a.whenPressed(new SetLiftEncoder(0));	
 	}
 }
