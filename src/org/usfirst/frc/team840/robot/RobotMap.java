cba5feb22b177b2b313690e94edf2fc77aac76f5
Miles P. Olson <mpolson64@gmail.com>
2015-02-09 21:41:05 -0800

    Neatened up a bit

--- src/org/usfirst/frc/team840/robot/RobotMap.java
+++ src/org/usfirst/frc/team840/robot/RobotMap.java
@@ -4,14 +4,14 @@ package org.usfirst.frc.team840.robot;
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
  * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
+ * floating around. Each peripheral is designated an array; formatting is as
+ * follows:
+ * Talon: Port, direction
  */
 public class RobotMap {
     
-    //Arrays for physical devices' parameters
-    public static final int[] driveMotorLeft = {0, 1};	//Port, direction
-    public static final int[] driveMotorRight = {1, -1};	//Port, direction
-    
+    public static final int[] driveMotorLeft = {0, 1};
+    public static final int[] driveMotorRight = {1, -1};
     public static final int[] intakeMotorLeft = {4, 1};
     public static final int[] intakeMotorRight = {5, -1};
     
