9e2262bf4d5b57f954e0d3ab5ee2d3b14a482746
sahith00 <sahithb00@gmail.com>
2015-02-11 19:06:01 -0800

    Created a developer mode
    We created a developer mode so that all the controls would be on one
    gamepad.

--- src/org/usfirst/frc/team840/robot/OI.java
+++ src/org/usfirst/frc/team840/robot/OI.java
@@ -11,6 +11,8 @@ import edu.art.frc.lib.util.GamepadButton;
  */
 public class OI {
 	
+	private boolean devMode = false;
+	
 	private Gamepad gamepad = new Gamepad(0);
 	private Gamepad gamepad2 = new Gamepad(1);
 	
@@ -21,23 +23,39 @@ public class OI {
 	public OI() {
 		GamepadButton leftStick = new GamepadButton(gamepad, "LEFT_JOYSTICK");
 		GamepadButton rightStick = new GamepadButton(gamepad, "RIGHT_JOYSTICK");
-		GamepadButton a2 = new GamepadButton(gamepad2, "A");
-		GamepadButton b2 = new GamepadButton(gamepad2, "B");
-		
 		GamepadButton x = new GamepadButton(gamepad, "X");
 		GamepadButton b = new GamepadButton(gamepad, "B");
 		GamepadButton y = new GamepadButton(gamepad, "Y");
 		GamepadButton a = new GamepadButton(gamepad, "A");
-				
-		leftStick.toggleWhenActive(new ArcadeDriveCreep());
-		rightStick.toggleWhenActive(new ArcadeDriveCreep());
-
-		a2.whileHeld(new RollIn());
-		b2.whileHeld(new RollOut());
-
-		a.whenPressed(new SetLiftBottomEncoder());
-		b.whenPressed(new SetLiftLoadToteEncoder());
-		x.whenPressed(new SlideOut());
-		y.whenPressed(new SlideIn());	
+		GamepadButton leftTrigger = new GamepadButton(gamepad, "LEFT_TRIGGER");
+		GamepadButton rightTrigger = new GamepadButton(gamepad, "RIGHT_TRIGGER");
+		
+		GamepadButton a2 = new GamepadButton(gamepad2, "A");
+		GamepadButton b2 = new GamepadButton(gamepad2, "B");
+		
+		if(devMode) {
+			leftStick.toggleWhenActive(new ArcadeDriveCreep());
+			rightStick.toggleWhenActive(new ArcadeDriveCreep());
+			
+			a.whenPressed(new SetLiftBottomEncoder());
+			b.whenPressed(new SetLiftLoadToteEncoder());
+			x.whenPressed(new SlideOut());
+			y.whenPressed(new SlideIn());
+			
+			leftTrigger.whenPressed(new RollOut());
+			rightTrigger.whenPressed(new RollIn());
+		}	
+		else {
+			leftStick.toggleWhenActive(new ArcadeDriveCreep());
+			rightStick.toggleWhenActive(new ArcadeDriveCreep());
+		
+			a.whenPressed(new SetLiftBottomEncoder());
+			b.whenPressed(new SetLiftLoadToteEncoder());
+			x.whenPressed(new SlideOut());
+			y.whenPressed(new SlideIn());
+			
+			a2.whileHeld(new RollIn());
+			b2.whileHeld(new RollOut());
+		}
 	}
 }
