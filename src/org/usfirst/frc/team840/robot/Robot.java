316fc74b0579cad46009d715c911b221fb1bc604
KenTheWhaleGoddess <kenpreiser@gmail.com>
2015-02-12 18:21:59 -0800

    FINALLY OMG
    works

--- src/org/usfirst/frc/team840/robot/Robot.java
+++ src/org/usfirst/frc/team840/robot/Robot.java
@@ -18,10 +18,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends IterativeRobot {
 
-	//public static Drivetrain drivetrain;
-	//public static Intake intake;
-	//public static Stacker stacker;
-	//public static PDP pdp;
+	public static Drivetrain drivetrain;
+	public static Intake intake;
+	public static Stacker stacker;
+	public static PDP pdp;
 	public static MyAccelerometer accelerometer;
 	
 	public static OI oi;
@@ -32,16 +32,19 @@ public class Robot extends IterativeRobot {
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    public void robotInit() {
+    @SuppressWarnings("deprecation")
+	public void robotInit() {
     	SmartDashboard.putNumber("Creep factor: ", 0.5);
     	SmartDashboard.putNumber("Lerp factor: ", 0.125);
     	
-    	//drivetrain = new Drivetrain();
-    	//intake = new Intake();
-    	//stacker = new Stacker();
-    	//pdp = new PDP();
+    	drivetrain = new Drivetrain();
+    	intake = new Intake();
+    	stacker = new Stacker();
+    	pdp = new PDP();
     	accelerometer = new MyAccelerometer();
     	oi = new OI();
+    	
+    	SmartDashboard.putDouble("XAcc", accelerometer.getXAcc());
 	}
 	
 	public void disabledPeriodic() {
