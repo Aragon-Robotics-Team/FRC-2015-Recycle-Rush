b9d1e844bc9b9eca9e9618d0504b420f23a02918
KenTheWhaleGoddess <kenpreiser@gmail.com>
2015-02-12 19:54:53 -0800

    Added init
    Will need LOTS of work later

--- src/org/usfirst/frc/team840/robot/commands/RunCamera.java
+++ src/org/usfirst/frc/team840/robot/commands/RunCamera.java
@@ -1,6 +1,10 @@
+package org.usfirst.frc.team840.robot.command;
+
+import org.usfirst.frc.team840.robot.subsystems.MyCamera;
+
 public class RunCamera extends Command {
 	
 	public RunCamera() {
-		
+		 
 	}
 }
--- src/org/usfirst/frc/team840/robot/subsystems/MyCamera.java
+++ src/org/usfirst/frc/team840/robot/subsystems/MyCamera.java
@@ -1,5 +1,11 @@
+package org.usfirst.frc.team840.robot.subsystem;
+
 public class MyCamera extends Subsystem {
+	USBCamera cam;
+	
 	public MyCamera() {
-		
+		cam = new USBCamera('Joe');
+		cam.setFPS(1);
 	}
+	
 }
