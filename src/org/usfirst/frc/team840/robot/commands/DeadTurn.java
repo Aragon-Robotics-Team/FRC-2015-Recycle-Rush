6c1c9c07d4725389f291f3ec9a0ad1d6083c836c
ethan546 <18levitte05@myahs.org>
2015-02-26 14:07:05 -0800

    Update and rename deadTurn.java to DeadTurn.java

---
+++ src/org/usfirst/frc/team840/robot/commands/DeadTurn.java
@@ -0,0 +1,16 @@
+//do stuff
+
+package org.usfirst.frc.team840.robot.commands;
+//import edu.wpi.first.wpilibj.*;
+
+public class DeadTurn extends Command {
+  private double ;
+  
+  public DeadTurn() {
+    requires(Robot.drivetrain);
+  }
+}
+
+protected void execute () {
+  
+}
