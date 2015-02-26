709baaff690efb4fa3611d388ca6a8ec91bb1a51
ethan546 <18levitte05@myahs.org>
2015-02-26 14:22:23 -0800

    wrote it

--- src/org/usfirst/frc/team840/robot/commands/DeadTurn.java
+++ src/org/usfirst/frc/team840/robot/commands/DeadTurn.java
@@ -4,13 +4,32 @@ package org.usfirst.frc.team840.robot.commands;
 //import edu.wpi.first.wpilibj.*;
 
 public class DeadTurn extends Command {
-  private double ;
+    private double time;
+    private String direction;
   
-  public DeadTurn() {
-    requires(Robot.drivetrain);
-  }
-}
+    public DeadTurn(double t, String whichDirection) {
+      requires(Robot.drivetrain);
+      time = t;
+      direction = whichDirection;
+      }
+  
 
-protected void execute () {
-  
+  public void initialize () {
+    Timer.delay(time);
+  }
+
+  public void execute () {
+    
+    if (direction.equals("left")){
+      Robot.drivetrain.setLeft(-1.0);
+      Robot.drivetrain.setRight(1.0);
+    } else if (direction.equals("right")) {
+      Robot.drivetrain.setLeft(1.0);
+      Robot.drivetrain.setRight(-1.0);
+    }
+    Timer.delay(time);
+    Robot.drivetrain.setLeft(0.0);
+    Robot.drivetrain.setRight(0.0);
+  }
+
 }
