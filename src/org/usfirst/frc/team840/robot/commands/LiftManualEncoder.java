6ae3659c2706880edb55f4afb2969cb479c4c54b
mpolson64 <mpolson64@gmail.com>
2015-03-02 16:51:45 -0800

    Added test case for manual lift control

--- src/org/usfirst/frc/team840/robot/commands/LiftManualEncoder.java
+++ src/org/usfirst/frc/team840/robot/commands/LiftManualEncoder.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team840.robot.commands;
 import org.usfirst.frc.team840.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *	The pulley that raises the lift is connected to a hall effect encoder. This command uses PID to set the lift to any position. The setpoint is dynamic and can be set by a joystick input.
@@ -25,6 +26,8 @@ public class LiftManualEncoder extends Command {
     protected void execute() {
     	setpoint += -0.5 * Robot.oi.getOperatorPad().getLeftY();	//Add the joystick value to the setpoint. Due to the speed at which commands are called, the max speed it 5 in. / sec 
     	Robot.stacker.setSetpoint(-1 * setpoint);	//Motor is connected backwards
+    	SmartDashboard.putNumber("Setpoint", Robot.stacker.getSetpoint());
+    	SmartDashboard.putNumber("Current Distance", Robot.stacker.getPosition());
     }
 
     // Make this return true when this Command no longer needs to run execute()
